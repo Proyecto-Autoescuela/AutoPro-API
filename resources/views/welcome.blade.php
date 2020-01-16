1b3762bebbdac1e1fae985a1be376fa463a34d31
Proyecto Autoescuela <proyectoautoescuelacev@gmail.com>
2020-01-16 13:07:19 +0100

    Panel de administrador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Panel de Admin</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PANEL DE ADMIN
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <img src="http://www.theappdate.es/static/media/cache/90/2b/902be27467f427b5130276689a06bedb.png"/>
             </div>
         </div>
     </body>
