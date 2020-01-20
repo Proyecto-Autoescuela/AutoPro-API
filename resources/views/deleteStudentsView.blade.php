17ffaaab597d52dc658973502ac1dadf7a5bf916
Proyecto Autoescuela <proyectoautoescuelacev@gmail.com>
2020-01-20 18:16:02 +0100

    Pruebas POST

--- resources/views/deleteStudentsView.blade.php
+++ resources/views/deleteStudentsView.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@php( $students = \App\Student::all())
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -7,7 +8,13 @@
             <div class="card">
                 <div class="card-header">ELIMINAR</div>
                 <div class="card-body">
-                    
+                    <select class="form-control mx-sm-3 mb-2" style="max-width: 41rem">
+                        @foreach($students as $s)
+                            <option>{{$s->id}}. {{$s->name}}</option>
+                        @endforeach
+                    </select>
+                    <p style="margin-left: 1rem"><input type="checkbox"/>  Se que voy a borrar un estudiante</p>
+                    <input style="margin-top: 1rem" type="button" class="btn btn-light btn-lg btn-block" value="ELIMINAR"/>
                     <input style="margin-top: 1rem" type="button" class="btn btn-light btn-lg btn-block" value="ATRAS" onclick="location.href = '{{ route('students') }}'"/>
                 </div>
             </div>
